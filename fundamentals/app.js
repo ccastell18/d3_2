4d074eceb95ebb26a7d59d3a6fb6f85b107c696c
Chris Castellano <c.castellano18@gmail.com>
2022-06-12 18:07:11 -0500

    scatterplot setup

--- fundamentals/app.js
+++ fundamentals/app.js
@@ -1,5 +1,13 @@
 //const pBrowser = document.querySelector('p');
-const body = d3.select('body').append('p');
+const el = d3
+  .select('body')
+  .append('p')
+  // .attr('class', 'foo')
+  // .attr('class', 'bar')
+  .classed('foo', true)
+  .classed('bar', true)
+  .text('Hello World')
+  .style('color', 'blue');
 
 // console.log(pBrowser);
-console.log(body);
+console.log(el);
