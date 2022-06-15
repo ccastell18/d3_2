7e2bdf2a308fb3b8b404c4cd7264ec96c55e9eb6
Chris Castellano <c.castellano18@gmail.com>
2022-06-14 21:38:22 -0500

    heat map

--- heatmap/app.js
+++ heatmap/app.js
@@ -25,6 +25,21 @@ async function draw(el, scale) {
       .scaleLinear()
       .domain(d3.extent(dataset))
       .range(['white', 'red']);
+  } else if (scale === 'quantize') {
+    colorScale = d3
+      .scaleQuantize()
+      .domain(d3.extent(dataset))
+      .range(['white', 'pink', 'red']);
+  } else if (scale === 'quantile') {
+    colorScale = d3
+      .scaleQuantile()
+      .domain(dataset)
+      .range(['white', 'pink', 'red']);
+  } else if (scale === 'threshold') {
+    colorScale = d3
+      .scaleThreshold()
+      .domain([45200, 135600])
+      .range(['white', 'pink', 'red']);
   }
 
   //rectangles
@@ -43,3 +58,6 @@ async function draw(el, scale) {
 }
 
 draw('#heatmap1', 'linear');
+draw('#heatmap2', 'quantize');
+draw('#heatmap3', 'quantile');
+draw('#heatmap4', 'threshold');
