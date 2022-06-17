1e83d491eb837d469355d443a75ca99c3d9c4808
Chris Castellano <c.castellano18@gmail.com>
2022-06-16 22:26:22 -0500

    metric done

--- Historgram/app.js
+++ Historgram/app.js
@@ -2,9 +2,6 @@ async function draw() {
   // Data
   const dataset = await d3.json('data.json');
 
-  const xAccessor = (d) => d.currently.humidity;
-  const yAccessor = (d) => d.length;
-
   // Dimensions
   let dimensions = {
     width: 800,
@@ -30,52 +27,68 @@ async function draw() {
     );
 
   //scales
-  const xScale = d3
-    .scaleLinear()
-    .domain(d3.extent(dataset, xAccessor))
-    .range([0, dimensions.ctrWidth])
-    .nice();
 
-  //bin
-  const bin = d3.bin().domain(xScale.domain()).value(xAccessor).thresholds(10);
+  function histogram(metric) {
+    const xAccessor = (d) => d.currently[metric];
+    const yAccessor = (d) => d.length;
 
-  //new dataset
-  const newDataset = bin(dataset);
-  const padding = 5;
+    const xScale = d3
+      .scaleLinear()
+      .domain(d3.extent(dataset, xAccessor))
+      .range([0, dimensions.ctrWidth])
+      .nice();
 
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(newDataset, yAccessor)])
-    .range([dimensions.ctrHeight, 0])
-    .nice();
+    //bin
+    const bin = d3
+      .bin()
+      .domain(xScale.domain())
+      .value(xAccessor)
+      .thresholds(10);
 
-  //draw bars
-  ctr
-    .selectAll('rect')
-    .data(newDataset)
-    .join('rect')
-    .attr('width', (d) => d3.max([0, xScale(d.x1) - xScale(d.x0) - padding]))
-    .attr('height', (d) => dimensions.ctrHeight - yScale(yAccessor(d)))
-    .attr('x', (d) => xScale(d.x0))
-    .attr('y', (d) => yScale(yAccessor(d)))
-    .attr('fill', '#01c5c4');
+    //new dataset
+    const newDataset = bin(dataset);
+    const padding = 5;
 
-  //labels
-  ctr
-    .append('g')
-    .classed('bar-labels', true)
-    .selectAll('text')
-    .data(newDataset)
-    .join('text')
-    .attr('x', (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
-    .attr('y', (d) => yScale(yAccessor(d)) - 10)
-    .text(yAccessor);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(newDataset, yAccessor)])
+      .range([dimensions.ctrHeight, 0])
+      .nice();
 
-  const xAxis = d3.axisBottom(xScale);
-  const xAxisGroup = ctr
-    .append('g')
-    .style('transform', `translateY(${dimensions.ctrHeight}px)`);
-  xAxisGroup.call(xAxis);
+    //draw bars
+    ctr
+      .selectAll('rect')
+      .data(newDataset)
+      .join('rect')
+      .attr('width', (d) => d3.max([0, xScale(d.x1) - xScale(d.x0) - padding]))
+      .attr('height', (d) => dimensions.ctrHeight - yScale(yAccessor(d)))
+      .attr('x', (d) => xScale(d.x0))
+      .attr('y', (d) => yScale(yAccessor(d)))
+      .attr('fill', '#01c5c4');
+    //labels
+    ctr
+      .append('g')
+      .classed('bar-labels', true)
+      .selectAll('text')
+      .data(newDataset)
+      .join('text')
+      .attr('x', (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+      .attr('y', (d) => yScale(yAccessor(d)) - 10)
+      .text(yAccessor);
+
+    const xAxis = d3.axisBottom(xScale);
+    const xAxisGroup = ctr
+      .append('g')
+      .style('transform', `translateY(${dimensions.ctrHeight}px)`);
+    xAxisGroup.call(xAxis);
+  }
+
+  d3.select('#metric').on('change', function (e) {
+    e.preventDefault();
+    histogram(this.value);
+  });
+
+  histogram('dewPoint');
 }
 
 draw();
