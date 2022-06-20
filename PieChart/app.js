ac6a04014ca69f9e21d0da2a25931f34267d5cfd
Chris Castellano <c.castellano18@gmail.com>
2022-06-19 21:17:24 -0500

    pie chart

--- PieChart/app.js
+++ PieChart/app.js
@@ -1,6 +1,6 @@
 async function draw() {
   // Data
-  const dataset = await d3.csv('data.csv')
+  const dataset = await d3.csv('data.csv');
 
   // Dimensions
   let dimensions = {
@@ -9,22 +9,85 @@ async function draw() {
     margins: 10,
   };
 
-  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
-  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2;
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2;
+
+  const radius = dimensions.ctrWidth / 2;
 
   // Draw Image
-  const svg = d3.select('#chart')
-    .append("svg")
-    .attr("width", dimensions.width)
-    .attr("height", dimensions.height)
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
 
-  const ctr = svg.append("g") // <g>
+  const ctr = svg
+    .append('g') // <g>
     .attr(
-      "transform",
+      'transform',
       `translate(${dimensions.margins}, ${dimensions.margins})`
-    )
+    );
 
   // Scales
+  const populatePie = d3
+    .pie()
+    .value((d) => d.value)
+    .sort(null);
+  const slices = populatePie(dataset);
+  const arc = d3.arc().outerRadius(radius).innerRadius(0);
+  const arcLabels = d3.arc().outerRadius(radius).innerRadius(200);
+
+  const colors = d3.quantize((t) => d3.interpolateSpectral(t), dataset.length);
+
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(dataset.map((element) => element.name))
+    .range(colors);
+
+  //call on path
+  const arcGroup = ctr
+    .append('g')
+    .attr(
+      'transform',
+      `translate(${dimensions.ctrHeight / 2}, ${dimensions.ctrWidth / 2})`
+    );
+
+  arcGroup
+    .selectAll('path')
+    .data(slices)
+    .join('path')
+    .attr('d', arc)
+    .attr('fill', (d) => colorScale(d.data.name));
+
+  //labels
+  const labelGroup = ctr
+    .append('g')
+    .attr(
+      'transform',
+      `translate(${dimensions.ctrHeight / 2}, ${dimensions.ctrWidth / 2})`
+    )
+    .classed('labels', true);
+
+  labelGroup
+    .selectAll('text')
+    .data(slices)
+    .join('text')
+    .attr('transform', (d) => `translate(${arcLabels.centroid(d)})`)
+    .call((text) =>
+      text
+        .append('tspan')
+        .style('font-weight', 'bold')
+        .attr('y', -4)
+        .text((d) => d.data.name)
+    )
+    .call((text) =>
+      text
+        .filter((d) => d.endAngle - d.startAngle > 0.25)
+        .append('tspan')
+        .attr('y', 9)
+        .attr('x', 0)
+        .text((d) => d.data.value)
+    );
 }
 
-draw()
+draw();
